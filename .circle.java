dca8c48c781d76711bd668de13ececfff77b8ce2
Nazik007 <99899134+Nazik007@users.noreply.github.com>
2022-05-26 14:48:58 +0300

    Create .circle.java

---
+++ .circle.java
@@ -0,0 +1,10 @@
+public class Circle {
+    public static void main(String[] args) {
+        final double PI = 3.14159265359d;
+        double r = 4;
+        double s;
+        s = PI * (r * r);
+
+        System.out.println(s);
+    }
+    }
